b9407f2fdf7df3e521e2aaaf90ae306b25f43453
spackle0 <spackle0@users.noreply.github.com>
2023-05-15 12:37:31 -0400

    initial model and some html

--- intertraq/models.py
+++ intertraq/models.py
@@ -1,4 +1,11 @@
 # Third Party Libraries
 from django.db import models
 
-# Create your models here.
+
+class Appointment(models.Model):
+    title = models.CharField(max_length=200)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
--- intertraq/views.py
+++ intertraq/views.py
@@ -1,4 +1,16 @@
-# Third Party Libraries
-from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Appointment
 
-# Create your views here.
+
+def appointments(request):
+    appts = Appointment.objects.all()
+    appointment_list = []
+    for appointment in appts:
+        appointment_list.append({
+            'id': appointment.id,
+            'title': appointment.title,
+            'start': appointment.start_time.isoformat(),
+            'end': appointment.end_time.isoformat()
+        })
+
+    return JsonResponse(appointment_list, safe=False)
