ba608c932b75c73cad5cec538b8b6285c7a326e6
spackle0 <spackle0@users.noreply.github.com>
2023-07-01 01:01:24 -0400

    phone number, rearranging, formatting

--- intertraq/models.py
+++ intertraq/models.py
@@ -1,24 +1,24 @@
-# Third Party Libraries
 from django.db import models
 from django.utils import timezone
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Recruiter(models.Model):
     name = models.CharField(max_length=200)
-    is_independent = models.BooleanField()
-    company = models.ForeignKey("Company", on_delete=models.SET_NULL, null=True, blank=True)
     email = models.EmailField(max_length=254, null=True, blank=True)
+    phone_number = PhoneNumberField(region="US", blank=True)
+    company = models.ForeignKey("Company", on_delete=models.SET_NULL, null=True, blank=True)
+    is_independent = models.BooleanField()
 
     def __str__(self):
-        return self.name
+        return f"{self.name} ({self.company.name})"
 
 
 class Company(models.Model):
     name = models.CharField(max_length=100)
     address = models.CharField(max_length=255, blank=True)
     industry = models.CharField(max_length=100, blank=True)
-
-    phone_number = models.CharField(max_length=15, blank=True)
+    phone_number = PhoneNumberField(region="US", blank=True)
     website = models.URLField(max_length=200, null=True, blank=True)
 
     def __str__(self):
@@ -29,12 +29,12 @@ class Job(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     job_url = models.URLField(max_length=200, null=True, blank=True)
+    posted_date = models.DateTimeField(auto_now_add=True)
     company = models.ForeignKey("Company", on_delete=models.CASCADE)
     recruiter = models.ForeignKey("Recruiter", on_delete=models.SET_NULL, null=True)
-    posted_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.title
+        return f"{self.title} ({self.company.name})"
 
 
 class Interview(models.Model):
@@ -42,16 +42,17 @@ class Interview(models.Model):
     purpose = models.CharField(max_length=200)
     date = models.DateTimeField()
     attendees = models.ManyToManyField("Recruiter", blank=True)
+    virtual_link = models.URLField(max_length=200, null=True, blank=True)
 
     def __str__(self):
         return f"Interview for {self.job.title} on {self.date}"
 
 
 class CommunicationLog(models.Model):
+    date = models.DateField(default=timezone.now)
     recruiter = models.ForeignKey("Recruiter", on_delete=models.CASCADE)
     job = models.ForeignKey("Job", on_delete=models.CASCADE, null=True, blank=True)
-    message = models.TextField()
-    date = models.DateField(default=timezone.now)
+    log_entry = models.TextField()
 
     def __str__(self):
-        return f"{self.recruiter.name} - {self.message[:50]}..."
+        return f"{self.date:%Y-%m-%d}: {self.recruiter.name} - {self.log_entry[:50]}"
