fd3911288f1a70f52e68210a66febfd6742bfb95
spackle0 <spackle0@users.noreply.github.com>
2024-03-23 00:42:48 -0400

    communication logs

--- interviews/urls.py
+++ interviews/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import company_list, company_detail
+
+from .views import communication_log_detail, communication_log_list, company_detail, company_list
 
 app_name = "interviews"
 
 urlpatterns = [
-    path('companies/', company_list, name='company_list'),
-    path('companies/<int:id>/', company_detail, name='company_detail'),
+    path("companies/", company_list, name="company_list"),
+    path("companies/<int:id>/", company_detail, name="company_detail"),
+    path("communication-logs/", communication_log_list, name="communication_log_list"),
+    path("communication-logs/<int:id>/", communication_log_detail, name="communication_log_detail"),
 ]
--- interviews/views.py
+++ interviews/views.py
@@ -1,13 +1,27 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404, render
 
-from .models import Company
+from .models import CommunicationLog, Company
 
 
 def company_list(request):
     companies = Company.objects.all()
-    return render(request, "interviews/company/company_list.html", {'companies': companies})
+    return render(request, "interviews/company/company_list.html", {"companies": companies})
 
 
 def company_detail(request, id):
     company = get_object_or_404(Company, pk=id)
-    return render(request, 'interviews/company/company_detail.html', {'company': company})
+    return render(request, "interviews/company/company_detail.html", {"company": company})
+
+
+def communication_log_list(request):
+    communication_logs = CommunicationLog.objects.all()
+    return render(
+        request, "interviews/communicationlog/communicationlog_list.html", {"communication_logs": communication_logs}
+    )
+
+
+def communication_log_detail(request, id):
+    communication_log = get_object_or_404(CommunicationLog, pk=id)
+    return render(
+        request, "interviews/communicationlog/communicationlog_detail.html", {"communication_log": communication_log}
+    )
